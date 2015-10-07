c6f940465d1208802c11d200a6bbe0745fcf69aa
Fabien RASSINIER <fabien.rassinier@smile.fr>
2015-10-07 22:24:41 +0200

    Fix JS

--- src/main/resources/javascript/jnt_acceptCookieBar.js
+++ src/main/resources/javascript/jnt_acceptCookieBar.js
@@ -1,15 +1,57 @@
-(function () {
-    $(function() {
+var app = (function (app, undefined) {
+  "use strict";
 
-        function setCookie(cname, cvalue, exdays) {
-            var d = new Date();
-            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-            var expires = "expires=" + d.toUTCString();
-            document.cookie = cname + "=" + cvalue + "; " + expires +";  path=/";
-        }
+  // Cookies namespace
+  app.cookies = app.cookies || {};
 
-        $('#accept_cookie .close').click(function() {
-            setCookie("accept_cookie", "true", 365);
-        });
-    });
-})();
+  // Deal with possible jQuery conflicts
+  var $ = app.$ || $;
+
+  /**
+   * Set cookie for X days
+   *
+   * @param expiresInDays Number of days
+   * @returns {setCookie} Set cookie for the current domain
+   */
+  var setCookieForXDays = function (expiresInDays) {
+
+    /**
+     * Set cookie for X days for the current domain
+     *
+     * @param cookieName Cookie name to set
+     * @param cookieValue Cookie value to set
+     */
+    var setCookie = function (cookieName, cookieValue) {
+      var date = new Date();
+      date.setTime(date.getTime() + (expiresInDays * 24 * 60 * 60 * 1000));
+      var expires = "expires=" + date.toUTCString();
+      document.cookie = cookieName + "=" + cookieValue + "; " + expires + ";  path=/";
+    };
+
+    return setCookie;
+  };
+
+  /**
+   * Shortcut to set a cookie for one year
+   */
+  var setCookieForOneYear = setCookieForXDays(365);
+
+  /**
+   * Set cookie which allow setting other cookies for current domain for one year
+   */
+  var setAcceptCookie = function () {
+    setCookieForOneYear("_accept_cookie", "true");
+  };
+
+  // Expose API
+  app.cookies.setAcceptCookie = setAcceptCookie;
+
+  $(function () {
+    // Let overriding $elm value
+    var $elm = app.cookies.$elm || $("#cookieMessage .close");
+    // Set cookie for current domain when user dismisses legal information
+    $elm.on("click", setAcceptCookie);
+  });
+
+  return app;
+}(app || {}));
